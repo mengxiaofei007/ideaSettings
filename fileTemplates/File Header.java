106a632f653368d7fd845f21607e422fb3710763
zhoujianxin <zhoujianxin@yxqiche.com>
2020-05-03 03:04:48 +0800

    IU-2019.2.4 <Administrator@1Z1FQC2-104153A Create baseRefactoring.xml, ide.general.xml, git.xml, git_toolbox_2.xml, project.default.xml, rainbow_brackets.xml, lombok-plugin.xml, File Header.java, Default.xml, completionMLRanking.xml, file.template.settings.xml, ignore.xml, code.style.schemes, find.xml, ui.lnf.xml, File Header.java, filetypes.xml, projectView.xml, keymap.xml, remote-servers.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, mygroup.xml, VisualVMLauncher.xml, databaseDrivers.xml, templates.xml, javarebel.xml, stringManipulation.xml, Default.xml, intellij-javadocs.xml, Default for Windows copy.xml, laf.xml, docker.xml, jdk.table.xml, path.macros.xml, web-browsers.xml, vcs.xml, editor.codeinsight.xml, toStringTemplates.xml, mybatis.xml, Mybatis_SQL.xml, editor.xml, uml.xml, usageView.xml, markdown_navigator.shared.xml, debugger.xml, updates.xml

---
+++ fileTemplates/File Header.java
@@ -0,0 +1,7 @@
+/**
+* @Description: TODO
+* @Package: ${PACKAGE_NAME}.${NAME}
+* @Author zhoujianxin
+* @date ${DATE} ${TIME}
+*
+*/
---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,7 @@
+/**
+* @Description: TODO
+* @Package: ${PACKAGE_NAME}.${NAME}
+* @Author ${USER}
+* @date ${DATE} ${TIME}
+*
+*/
